abf3eebd66dfa791a89962ff6cd824772a8832ec
HugoDominguezF <hugo.dominguez@iesnervion.es>
2025-10-08 10:06:16 +0200

    asd

---
+++ Clases/Ejercicio3.py
@@ -0,0 +1,24 @@
+
+import math
+
+class Punto:
+
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+
+    def setXY(self,x,y):
+        self.x = x
+        self.y = y
+    
+    def desplaza(self,dx,dy):
+        self.x += dx
+        self.y += dy
+    
+    def distancia(self,punto):
+        distancia = math.sqrt((punto.x-self.x)**2+(punto.y-self.y)**2)
+        return distancia
+
+    def __str__(self):
+        return f"({self.x}, {self.y})"
+    
---
+++ Clases/Ejercicio4.py
@@ -0,0 +1,38 @@
+class Articulo:
+
+    IVA = 21
+
+    def __init__(self,nombre,precio,cuantosQuedan):
+        self.nombre = nombre
+        self.precio = precio
+        self.cuantosQuedan = cuantosQuedan
+
+    def getPVP(self):
+        return self.precio+(self.precio*self.IVA)
+    
+    def getPVPDescuento(self,descuento):
+        if descuento<101 and descuento>0:
+            conDescuento = (self.precio+(self.precio*self.IVA)*((100-descuento)/100))
+    
+    def vender(self,cantidad):
+        if self.cuantosQuedan >= cantidad and cantidad>0:
+            self.cuantosQuedan -= cantidad
+            sePudo = True
+        else:
+            sePudo = False
+        return sePudo
+
+    def almacenar(self,cantidad):
+        if cantidad>0:
+            self.cuantosQuedan+=cantidad
+        
+
+    def __str__(self):
+        return f"El articulo {self.nombre} que cuesta {self.precio} tiene un stock de {self.cuantosQuedan}"
+    
+    def __eq__(self, value):
+        return (self.nombre==value.nombre)
+    
+    def __lt__(self,value):
+        return self.nombre<value.nombre
+    
